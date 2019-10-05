cc9b05b22481e64dcfe0c113dfa64f2de5ea326e
manjunathkumbar <manjunathkumbar393@gmail.com>
2019-10-05 22:43:44 +0530

    functions

--- recursion.go
+++ recursion.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(4 * 3 * 2 * 1)
+	//fmt.Println(4 * 3 * 2 * 1)
 	n := factorial(4)
 	fmt.Println(n)
+	m:=loopfact(5)
+	fmt.Println(m)
 }
 
 func factorial(n int) int {
@@ -13,4 +15,12 @@ func factorial(n int) int {
 		return 1
 	}
 	return n * factorial(n-1)
+}
+
+func loopfact(m int)  int {
+	total:=1
+	for; m>0; m--{
+		total *=m
+	}
+	return total
 }
